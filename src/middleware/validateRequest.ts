35faac13ceb81e64f538bfe5482a8e15070279b7
Lobinda <lobinda123@gmail.com>
2025-02-14 13:05:22 +0800

    feature/建立req資料zod驗證套版

--- src/middleware/validateRequest.ts
+++ src/middleware/validateRequest.ts
@@ -1,20 +1,29 @@
 import { Request, Response, NextFunction } from 'express';
-import { ZodSchema } from 'zod';
+import { ZodError, ZodSchema } from 'zod';
+import { appError } from '../utils/appResponse';
+
+type ReqPropertyType = 'body' | 'params' | 'query';
 
 /**
- * validateRequest middleware
- * @param schema - Zod 驗證 schema
- * @returns 一個 middleware，用來驗證 req.body，驗證通過則呼叫 next()，否則傳遞錯誤
+ * 驗證資料
+ * @param schema - Zod schema
+ * @param property -  'body' | 'params' | 'query'
+ * @returns 驗證通過next()，失敗傳遞錯誤
  */
-const validateRequest = (schema: ZodSchema<any>) => async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    console.log(req);
-    req.body = await schema.parseAsync(req.body);
-    next();
-  } catch (error) {
-    console.log(1);
-    next(error);
-  }
+export const validateData = <T>(schema: ZodSchema<T>, property: ReqPropertyType) => {
+  return async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+      req[property] = await schema.parseAsync(req[property]);
+      next();
+    } catch (error) {
+      if (error instanceof ZodError) {
+        const firstErrorMessage = error.errors[0]?.message || '系統錯誤';
+        console.error(firstErrorMessage);
+        // 傳遞錯誤訊息給 appError
+        appError(firstErrorMessage, next, 400);
+      } else {
+        appError('validate處理異常', next);
+      }
+    }
+  };
 };
-
-export default validateRequest;
