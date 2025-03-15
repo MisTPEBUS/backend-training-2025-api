8c190303ca68e99e23cba62829bac8890f79b4f4
Lobinda <lobinda123@gmail.com>
2025-03-16 01:30:04 +0800

    update week5

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import express, { Application, Request, Response } from 'express';
@@ -19,9 +20,38 @@ app.use(bodyParser.json());
 app.use('/api', routers);
 
 app.get('/OPTION', (req: Request, res: Response) => {
-  res.status(200).json();
+  return res.status(200).json();
 });
 
+//設定bot
+app.post('/sendMsg', async (req: Request, res: Response) => {
+  const TELEGRAM_TOKEN = process.env.TELEGRAM_TOKEN;
+  if (!TELEGRAM_TOKEN) {
+    return res.status(500).json({ error: '請在環境變數中設定 TELEGRAM_TOKEN' });
+  }
+
+  const TELEGRAM_API = `https://api.telegram.org/bot${TELEGRAM_TOKEN}`;
+
+  // 從請求中取得 chatId 與 text
+  const { chat_id, text } = req.body;
+  console.log(chat_id);
+  if (!chat_id || !text) {
+    return res.status(400).json({ error: '請提供 chatId 與 text 參數' });
+  }
+
+  try {
+    const url = `${TELEGRAM_API}/sendMessage`;
+    const response = await axios.post(url, {
+      chat_id: chat_id,
+      text: text,
+    });
+    console.log('訊息發送結果:', response.data);
+    return res.status(200).json(response.data);
+  } catch (error) {
+    console.error('訊息發送失敗:', error);
+    return res.status(500).json({ error: '訊息發送失敗', details: error });
+  }
+});
 //Route 404
 app.use(NotFound);
 
